ad32621576acd32faace72ce4323139326a9a7ba
Omar Guerrero <Guerrero Software>
2023-07-05 22:10:48 -0700

    adding ad config

--- ActiveDirectoryManager.psm1
+++ ActiveDirectoryManager.psm1
@@ -1,7 +1,32 @@
-class ActiveDirectoryManger {
+Import-Module ADDSDeployment
+class ActiveDirectoryManager {
     
-    ActiveDirectoryManger()
+    ActiveDirectoryManager(){}
+    InstallDomainServices()
     {
-        
+        $statusAD = Get-WindowsFeature -Name AD-Domain-Services
+        if(-not $statusAD.Installed)
+        {
+        Add-WindowsFeature AD-Domain-Services
+        }
+    }
+
+    SetUpDomainServices([string]$DomainName)
+    {
+        $domain = [uri]$DomainName
+        $domainNetbiosName = $domain.Host -replace '^www\.' -replace '\..+$'
+
+            Install-ADDSForest `
+            -CreateDnsDelegation:$false `
+            -DatabasePath "C:\Windows\NTDS" `
+            -DomainMode "WinThreshold" `
+            -DomainName $DomainName `
+            -DomainNetbiosName $domainNetbiosName `
+            -ForestMode "WinThreshold" `
+            -InstallDns:$true `
+            -LogPath "C:\Windows\NTDS" `
+            -NoRebootOnCompletion:$false `
+            -SysvolPath "C:\Windows\SYSVOL" `
+            -Force:$true
     }
 }
--- Invoke-ActiveDirectoryCreation.ps1
+++ Invoke-ActiveDirectoryCreation.ps1
@@ -1,9 +1,20 @@
 using module .\DNSNetworkInterface.psm1
 using module .\DNSNetworkInterfacesManager.psm1
+using module .\ActiveDirectoryManager.psm1
 function Invoke-ActiveDirectoryCreation() {
-    [DNSNetworkInterfacesManager]$ni = [DNSNetworkInterfacesManager]::new()
-    $SelectedInterfaceIndex  = ""
-    $canContinue = $false
+        [DNSNetworkInterfacesManager]$ni = [DNSNetworkInterfacesManager]::new()
+        [ActiveDirectoryManager]$activeDirecoryManager = [ActiveDirectoryManager]::new()
+        $SelectedInterfaceIndex  = ""
+        $canContinue = $false
+        $serverName = $env:computername
+        $SelectedNameOption  = Read-Host  "Prompt The name of the computer is "  $serverName  ", do you want to change it?"
+        if($SelectedNameOption -eq "y")
+        {
+                $newNameServer = Read-Host  "Insert the new name(the computer will restart): "
+                Rename-Computer -NewName $newNameServer
+                Restart-Computer
+        }
+        $canContinue = $false
         do{
             $interfaces = $ni.GetDNSNetworkInterfaces()
             $interfaces | Format-Table
@@ -16,7 +27,10 @@ function Invoke-ActiveDirectoryCreation() {
         }while ($true -ne $canContinue)
         $resultStaticIP = $ni.SetStaticIPDNS($SelectedInterfaceIndex)
         Write-Host($resultStaticIP)
-        
+        $activeDirecoryManager.InstallDomainServices();
+        Write-Host("Domain Services Installed")
+        $newDomainName = Read-Host  -Prompt  "Insert the new domain "
+        $activeDirecoryManager.SetUpDomainServices($newDomainName)
 }
 
 
